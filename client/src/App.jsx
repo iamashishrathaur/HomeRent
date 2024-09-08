757dca2aae548def2330cb6edc7179cbe614720f
Ashish Rathaur <121791024+iamashishrathaur@users.noreply.github.com>
2024-09-08 20:42:54 +0530

    update react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from 'react'
 import './App.css'
 import Home from './client/Home'
 import LoginForm from './LoginForm'
+ import { RouterProvider ,createBrowserRouter }  from 'react-router-dom'
 
 function App() {
+
+  const routes = createBrowserRouter([{
+    element:<LoginForm/>,
+    path:'/login'
+  },
+  {
+    element:<Home/>,
+    path:'/'
+  }
+   ])
 
   return (
     <div className='w-full bg-slate-50'>
        {/* <Home/> */}
-       <LoginForm/>
+       {/* <LoginForm/> */}
+       <RouterProvider router={routes}/>
     </div>
   )
 }
--- client/src/LoginForm.jsx
+++ client/src/LoginForm.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+  const navigate = useNavigate();
   return (
     <div className="relative w-full h-screen bg-white flex items-center justify-center overflow-hidden">
       <div className="absolute w-[280px] h-[400px]">
         <div className="absolute h-[200px] w-[200px] rounded-full bg-gradient-to-r from-[#1845ad] to-[#23a2f6] -top-[80px] -left-[80px]"></div>
         <div className="absolute h-[200px] w-[200px] rounded-full bg-gradient-to-r from-[#ff512f] to-[#f09819] -bottom-[80px] -right-[80px]"></div>
       </div>
-      <form className="relative w-[400px] h-[520px] bg-white bg-opacity-10 backdrop-blur-md rounded-[10px] p-[50px_35px] border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)]">
+      <form className="relative w-[400px] h-[520px] bg-white bg-opacity-10 backdrop-blur-md rounded-[10px] p-[50px_35px] border border-white/10 shadow-[0_0_40px_rgba(8,7,16,0.6)]" >
         <h3 className="text-center text-white text-[32px] font-medium leading-[42px]">Login <span className="text-blue-500">Here</span></h3>
 
         <label htmlFor="username" className="block mt-[30px] text-black text-[16px] font-medium">
@@ -30,7 +32,7 @@ const LoginForm = () => {
           className="block w-full h-[50px] bg-white bg-opacity-10 rounded-[3px] border-2 border-[black] p-[0_10px] mt-[8px] outline-none text-[14px] font-light text-black placeholder-gray-500"
         />
 
-        <button className="w-full mt-[50px] bg-white border shadow-lg text-[#080710] py-[15px] text-[18px] font-semibold rounded-[5px] hover:bg-gray-200 transition-all">
+        <button className="w-full mt-[50px] bg-white border shadow-lg text-[#080710] py-[15px] text-[18px] font-semibold rounded-[5px] hover:bg-gray-200 transition-all" onClick={()=>navigate('/')}>
           Log In
         </button>
       </form>
